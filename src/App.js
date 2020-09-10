6de2991f8fef082174f07477c1ad64f8b1cb5c44
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-09-10 17:19:21 -0400

    initial setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Weather App</h1>
+      </header>
+      <main></main>
+      <footer>App created by Arek Chatmajian</footer>
+    </div>
+  );
+}
+
+export default App;
